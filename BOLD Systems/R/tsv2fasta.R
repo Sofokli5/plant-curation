0d40d252f480b0cc770608e2e8dede4339b1c417
Sofoklis Keisaris <sofoskeisaris@gmail.com>
2025-12-03 12:28:42 +0200

    test drive adjustments

--- BOLD Systems/R/tsv2fasta.R
+++ BOLD Systems/R/tsv2fasta.R
@@ -26,18 +26,20 @@ tsv2fasta <- function(input_sequences_table, output_dir) {
 
   its0[, tax_col := paste(kingdom, phylum, class, order, family, genus, species, sep = ";")]
   trnl[, tax_col := paste(kingdom, phylum, class, order, family, genus, species, sep = ";")]
-
+  its0[, fasta_col := paste0(record_id, "_", tax_col)]
+  trnl[, fasta_col := paste0(record_id, "_", tax_col)]
+  
   # workflow -----------------------
 
   dir.create(output_dir, showWarnings = FALSE)
 
-  write.fasta(sequences = as.list(its0$nuc), names = as.list(its0$processid), file.out = paste0(output_dir, "/ITS0.fasta"), nbchar = 120)
-  write.fasta(sequences = as.list(trnl$nuc), names = as.list(trnl$processid), file.out = paste0(output_dir, "/trnL.fasta"), nbchar = 120)
+  write.fasta(sequences = as.list(its0$nuc), names = as.list(its0$fasta_col), file.out = paste0(output_dir, "/ITS0.fasta"), nbchar = 120)
+  write.fasta(sequences = as.list(trnl$nuc), names = as.list(trnl$fasta_col), file.out = paste0(output_dir, "/trnL.fasta"), nbchar = 120)
 
-  R.utils::gzip(paste0(output_dir, "/ITS0.fasta"))
+  #R.utils::gzip(paste0(output_dir, "/ITS0.fasta"))
   R.utils::gzip(paste0(output_dir, "/trnL.fasta"))
 
-  # its0_tax <- its0[, .(processid, tax_col)]
+  its0_tax <- its0[, .(processid, tax_col)]
   trnl_tax <- trnl[, .(processid, tax_col)]
 
   # fwrite(its0_tax, "./r-curation/ITS0.tax", sep = "\t", row.names = FALSE, col.names = F, quote = FALSE)
